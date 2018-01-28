a5d94ee4100400945b2b7806bde0fa6511234ae3
Joseph <josephsantanapr@gmail.com>
2018-01-28 11:21:43 -0600

    Adding CNAME File

--- js/index.js
+++ js/index.js
@@ -55,7 +55,7 @@ function DrawTable(){
   var x = document.getElementById("container");
   x.style.display = "none";
   var table = document.getElementById("tableDisp");
-  x.style.display = "block"
+  x.style.display = "show"
   var row = table.insertRow(-1);
   var cell1 = row.insertCell(0);
   cell1.innerHTML = "Sriram";
